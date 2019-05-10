9758c7697348ff3494c13e6335bb05247fe73d8d
ArtMin96 <artminasyanart96@inbox.ru>
2019-05-10 13:49:31 +0400

    refactoring newsfeed page, navbar, left sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,10 @@
-// Chart.js
-
-var likesChart = document.getElementById('likesChart').getContext('2d');
-var chart = new Chart(likesChart, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Likes chart',
-            backgroundColor: 'rgb(0, 123, 255)',
-            borderColor: 'rgb(0, 123, 255)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
-var followersChart = document.getElementById('followersChart').getContext('2d');
-var myPieChart = new Chart(followersChart, {
-    type: 'pie',
-    data: {
-        labels: [
-            'Followers',
-            'Following'
-        ],
-        datasets: [{
-            backgroundColor: ['rgb(0, 123, 255)', 'rgb(50,205,50)'],
-            data: [32, 17]
-        }]
-    },
-    // Configuration options go here
-    options: {}
-});
+//$(document).ready(function () {
+//    var isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+//    var anchors = document.querySelectorAll('link[href*="assets/css/style.css"]');
+//
+//    if (isMobile) {
+//        Array.prototype.forEach.call(anchors, function (element, index) {
+//            element.href = "assets/css/media.css";
+//        });
+//    }
+//});
