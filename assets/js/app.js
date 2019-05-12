e4e3c21a2288dccd365c72bf5d027c301517bb6b
ArtMin96 <artminasyanart96@inbox.ru>
2019-05-12 06:06:17 +0400

    Added new chat popup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,4 @@
-//$(document).ready(function () {
-//    var isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
-//    var anchors = document.querySelectorAll('link[href*="assets/css/style.css"]');
-//
-//    if (isMobile) {
-//        Array.prototype.forEach.call(anchors, function (element, index) {
-//            element.href = "assets/css/media.css";
-//        });
-//    }
-//});
-
+// Real time detect browser screen size
 (function (window) {
     window.watchResize = function (callback) {
         var resizing;
@@ -18,7 +8,7 @@
             var curr_size = window.innerWidth;
             clearTimeout(resizing);
             resizing = null;
-            // only run on a true resize
+            // Only run on a true resize
             if (callback.size != curr_size) {
                 callback();
                 callback.size = curr_size;
@@ -36,26 +26,69 @@
     };
 }(window));
 
-// watch browser width on resize
+// Watch browser width on resize
 var browser_width = 0;
 window.watchResize(function () {
     browser_width = window.innerWidth || document.body.offsetWidth;
 });
 
-// do stuff after breakpoint
+// Do stuff after breakpoint
 window.watchResize(function () {
     var threshold = 992;
     if (browser_width < threshold) {
-        // do stuff when window is ≥ 992px wide
+        // Do stuff when window is ≥ 992px wide
         $('.message-drop-li').hide();
+        $('#hide-in-mobile').hide();
         $('.notification').removeClass('show');
         $(".rm-drop-mobile").removeAttr("data-toggle");
         $(".notification a.nav-link").attr("href", "notification.html");
     } else {
         $('.message-drop-li').show();
+        $('#hide-in-mobile').show();
         $('.notification').addClass('show');
         $(".rm-drop-mobile").attr("data-toggle", "dropdown");
     }
 });
 
+// Create dropdown and tooltip together
 $('.drop-w-tooltip').tooltip();
+
+// Chatbox
+
+// Minimize chat box
+$('#minimize-chat-window').on('click', function () {
+    $('#messagebody').slideToggle();
+    $('#messagebody').animate({
+        scrollTop: $('#messagebody').offset().top
+    }, 500);
+});
+
+// Close chatbox
+$('#close-chatbox').on('click', function () {
+    $('.chat-popup').hide();
+});
+
+// Chat body always keep to bottom
+$('#messagebody').animate({
+    scrollTop: $('#messagebody').prop('scrollHeight')
+});
+
+// Send message with Enter key
+$('.type-message').on("keydown", function (event) {
+    var keycode = event.keyCode || event.which;
+    var chat = $(".type-message").val();
+    if (keycode == '13') {
+        var body = '<div class="row msg_container base_sent">' +
+            '<div class="col-md-10 col-xs-10 ">' +
+            '<div class="messages message-reply bg-primary shadow-sm">' +
+            '<p>' + chat + '</p>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+        $(".type-message").val("");
+        $(body).appendTo("#messagebody");
+        $("#messagebody").animate({
+            scrollTop: $("#messagebody")[0].scrollHeight
+        }, 'slow');
+    }
+});
